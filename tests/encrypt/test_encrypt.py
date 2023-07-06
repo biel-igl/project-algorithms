9bc3cfa74854f4a5df2ce8782f4fc5c97eaaeb5f
biel-igl <gabrielpires524@gmail.com>
2023-07-06 18:59:54 -0300

    feat: faz req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(3, "gabrie")
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("oi", "gabrie")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 5)
+    assert encrypt_message("gabrie", 3) == "bag_eir"
+    assert encrypt_message("gabriel", 4) == "lei_rbag"
+    assert encrypt_message("gabriel", 77) == "leirbag"
