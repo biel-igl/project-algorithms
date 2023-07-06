61a8a710ed20ab2600d1ad72a3ef9b697c0f1010
biel-igl <gabrielpires524@gmail.com>
2023-07-06 16:58:41 -0300

    feat: faz req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    period_dict = {}
+    for this_tuple in permanence_period:
+        if not (
+            isinstance(this_tuple[0], int)
+            and isinstance(this_tuple[1], int)
+            and isinstance(target_time, int)
+        ):
+            return None
+        else:
+            lista = list(range(this_tuple[0], this_tuple[1] + 1))
+            for number in lista:
+                if number in period_dict:
+                    period_dict[number] += 1
+                else:
+                    period_dict[number] = 1
+    return period_dict[target_time]
