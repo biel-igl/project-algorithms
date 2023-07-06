880df3ef2d4b10199ae3d1efdf4ab50cdb5b0eb1
biel-igl <gabrielpires524@gmail.com>
2023-07-06 19:36:34 -0300

    feat: faz exercicio 06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,4 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    return list(word) == list(reversed(word))
