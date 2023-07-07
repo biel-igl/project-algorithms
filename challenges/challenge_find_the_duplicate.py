490ab14d40ab0f40ee48458f81d484195cfa27d2
biel-igl <gabrielpires524@gmail.com>
2023-07-06 21:58:57 -0300

    feat: faz req 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
+import collections
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums:
+        return False
+    for number in nums:
+        if not isinstance(number, int) or number < 0:
+            return False
+    duplicate_dict = collections.Counter(nums)
+    sorted_dict = sorted(
+        duplicate_dict.items(), key=lambda items: items[1], reverse=True
+    )
+    if sorted_dict[0][1] == 1:
+        return False
+    return sorted_dict[0][0]
