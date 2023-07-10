9d47d54b6f0cc61333cfc51bb0a881ef869224a3
biel-igl <gabrielpires524@gmail.com>
2023-07-10 16:43:24 -0300

    feat: faz req 04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,27 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_first = counting_sort(first_string.lower()) if first_string else ""
+    sorted_second = (
+        counting_sort(second_string.lower()) if second_string else ""
+    )
+
+    if not first_string or not second_string:
+        return sorted_first, sorted_second, False
+
+    if len(sorted_first) != len(sorted_second):
+        return sorted_first, sorted_second, False
+
+    return sorted_first, sorted_second, sorted_first == sorted_second
+
+
+def counting_sort(string):
+    counts = [0] * 26  # Considerando apenas as letras do alfabeto
+
+    for char in string:
+        if "a" <= char <= "z":
+            counts[ord(char) - ord("a")] += 1
+
+    sorted_string = []
+    for i in range(26):
+        sorted_string.extend([chr(i + ord("a"))] * counts[i])
+
+    return "".join(sorted_string)
