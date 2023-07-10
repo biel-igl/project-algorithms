f2d51c70231b9016b5a0d5c88c6ba13d9aec6f5f
biel-igl <gabrielpires524@gmail.com>
2023-07-10 10:45:53 -0300

    feat: faz req 03

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -8,9 +8,7 @@ def find_duplicate(nums):
         if not isinstance(number, int) or number < 0:
             return False
     duplicate_dict = collections.Counter(nums)
-    sorted_dict = sorted(
-        duplicate_dict.items(), key=lambda items: items[1], reverse=True
-    )
-    if sorted_dict[0][1] == 1:
+    most_common = duplicate_dict.most_common(1)
+    if most_common[0][1] == 1:
         return False
-    return sorted_dict[0][0]
+    return most_common[0][0]
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    return low_index >= high_index or (
+        word[low_index] == word[high_index]
+        and is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    )
